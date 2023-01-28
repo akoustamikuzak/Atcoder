32eae1fef896c4947fe8d9704e0a37f783eefe36
松岡和希 <akoustam0708kaz@gmail.com>
2023-01-28 16:18:44 +0900

    Atcoderプロジェクト作成

---
+++ src/math_algorithm_book/prob2_1_3.java
@@ -0,0 +1,15 @@
+package math_algorithm_book;
+import java.util.*;
+
+class Prob2_1_3 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A1 = sc.nextInt();
+        int A2 = sc.nextInt();
+        int A3 = sc.nextInt();
+
+        System.out.println(A1 * A2 * A3);
+    }
+}
